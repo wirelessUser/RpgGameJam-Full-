213dd20b4c99fe62973f0b5cf7a89e66f3bb72bb
Abi <97323239+wirelessContri@users.noreply.github.com>
2023-04-05 19:48:37 +0530

    Asset11 enum and item detailed script crteated

---
+++ Assets/Scripts/Enum/enum.cs
@@ -0,0 +1,34 @@
+public enum ToolEffect
+{
+    none,
+   watering
+}
+
+
+public enum Direction
+{
+    up,
+    right,
+    left,
+    down,
+    none
+}
+
+
+public enum itemType 
+
+{ 
+    Seed,
+    Commodity,
+    Watering_Tool,
+    Hoing_Tool,
+    Chopping_Tool,
+    Breaking_tool,
+    Reaping_Tool,
+    Collecting_tool,
+    Reapable_Scenary,
+    none,
+    count
+
+}
+
